9244eec4b788a3fb82cd2a335d21dcc43f619444
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2016-10-13 11:58:58 +0100

    Add init method to user object

--- app/user/models.py
+++ app/user/models.py
@@ -1,5 +1,7 @@
 from app import db
 
+from flask_bcrypt import generate_password_hash
+
 
 class User(db.Model):
     """Represents a user."""
@@ -9,3 +11,12 @@ class User(db.Model):
     last_name = db.Column(db.String(120))
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.LargeBinary())
+
+    def __init__(self, first_name, last_name, email, password):
+        """Create a User object but not save it to the database."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+
+        # Securely hash the password using bcrypt
+        self.password = generate_password_hash(password)
