8d16145e3d585bd0a3a03c2434575ae22fdc266b
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2016-10-13 15:39:04 +0100

    Add username column

--- app/user/models.py
+++ app/user/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     """Represents a user."""
 
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
     first_name = db.Column(db.String(120))
     last_name = db.Column(db.String(120))
     email = db.Column(db.String(120), unique=True)
@@ -20,3 +21,13 @@ class User(db.Model):
 
         # Securely hash the password using bcrypt
         self.password = generate_password_hash(password)
+
+    def to_dict(self):
+        """Convert instance into a dict, excluding password."""
+        return {
+            'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email,
+            'is_ad_free': self.is_ad_free
+        }
