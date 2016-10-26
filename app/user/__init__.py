69fe080622d52d3981a9872fe4ad4391cf46108f
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2016-10-26 14:39:19 +0100

    Refactor

--- app/user/__init__.py
+++ app/user/__init__.py
@@ -1,12 +1,9 @@
 from flask import Blueprint, request, jsonify, g
 from flask_jwt import jwt_required
 
-from app.exceptions import FieldInUseError, CustomError
-from app.helper import json_from_request, check_keys, get_boolean_query_param
 from app.permissions.decorators import permissions_required
-from app.school.models import School
 
-from .models import db, User
+from .functions import user_listing, user_create
 
 user_blueprint = Blueprint("user", __name__, url_prefix="/user")
 
@@ -19,51 +16,10 @@ def index():
 @user_blueprint.route("/user", methods=["GET", "POST"])
 @jwt_required()
 @permissions_required({'CRUD_USERS'})
-def create_user():
+def user_listing_or_create():
     """Route to create User from a POST request."""
     if request.method == "GET":
-        nest_roles = get_boolean_query_param(request, 'nest-roles')
-        nest_role_permissions = get_boolean_query_param(request, 'nest-role-permissions')
-        nest_permissions = get_boolean_query_param(request, 'nest-permissions')
-
-        users = User.query.filter_by(school_id=g.user.school_id)
-        users_list = [
-            u.to_dict(
-                nest_roles=nest_roles,
-                nest_role_permissions=nest_role_permissions,
-                nest_permissions=nest_permissions
-            ) for u in users
-        ]
-        return jsonify({'success': True, 'users': users_list})
+        return user_listing(request)
 
     if request.method == "POST":
-        # Decode the JSON data
-        data = json_from_request(request)
-
-        # Validate data
-        expected_keys = ["first_name", "last_name", "password", "username", "email", "school_id"]
-        check_keys(expected_keys, data)
-
-        if School.query.filter_by(id=data['school_id']).first() is None:
-            raise CustomError(401, message="School with id: {} does not exist.".format(data['school_id']))
-
-        if User.query.filter_by(email=data['email']).first() is not None:
-            raise FieldInUseError("email")
-
-        if User.query.filter_by(username=data['username'], school_id=data['school_id']).first() is not None:
-            raise FieldInUseError("username")
-
-        # Create user
-        user = User(
-            first_name=data['first_name'],
-            last_name=data['last_name'],
-            email=data['email'],
-            password=data['password'],
-            username=data['username'],
-            school_id=data['school_id']
-        )
-
-        db.session.add(user)
-        db.session.commit()
-
-        return jsonify({"success": True, "user": user.to_dict()}), 201
+        return user_create(request)
---
+++ app/user/functions.py
@@ -0,0 +1,57 @@
+from flask import jsonify, g
+
+from app import db
+from app.exceptions import CustomError, FieldInUseError
+from app.helper import get_boolean_query_param, json_from_request, check_keys
+from app.school.models import School
+
+from .models import User
+
+
+def user_listing(request):
+    nest_roles = get_boolean_query_param(request, 'nest-roles')
+    nest_role_permissions = get_boolean_query_param(request, 'nest-role-permissions')
+    nest_permissions = get_boolean_query_param(request, 'nest-permissions')
+
+    users = User.query.filter_by(school_id=g.user.school_id)
+    users_list = [
+        u.to_dict(
+            nest_roles=nest_roles,
+            nest_role_permissions=nest_role_permissions,
+            nest_permissions=nest_permissions
+        ) for u in users
+        ]
+    return jsonify({'success': True, 'users': users_list})
+
+
+def user_create(request):
+    #  Decode the JSON data
+    data = json_from_request(request)
+
+    # Validate data
+    expected_keys = ["first_name", "last_name", "password", "username", "email", "school_id"]
+    check_keys(expected_keys, data)
+
+    if School.query.filter_by(id=data['school_id']).first() is None:
+        raise CustomError(401, message="School with id: {} does not exist.".format(data['school_id']))
+
+    if User.query.filter_by(email=data['email']).first() is not None:
+        raise FieldInUseError("email")
+
+    if User.query.filter_by(username=data['username'], school_id=data['school_id']).first() is not None:
+        raise FieldInUseError("username")
+
+    # Create user
+    user = User(
+        first_name=data['first_name'],
+        last_name=data['last_name'],
+        email=data['email'],
+        password=data['password'],
+        username=data['username'],
+        school_id=data['school_id']
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({"success": True, "user": user.to_dict()}), 201
