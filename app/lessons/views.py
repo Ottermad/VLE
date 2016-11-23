ac7cab9b4da2646eeb002cae94dd0a77cd59f56d
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2016-11-23 14:41:14 +0000

    Add roles

--- app/lessons/views.py
+++ app/lessons/views.py
@@ -1,7 +1,9 @@
+from app.exceptions import UnauthorizedError
 from app.lessons.subject_functions import create_subject_view, list_subject_view, subject_detail_view, subject_delete_view, \
     subject_update_view
 from app.lessons.lesson_functions import lesson_create, lesson_listing, lesson_detail, lesson_delete, lesson_update
 from flask import Blueprint, request
+from flask.globals import g
 from flask_jwt import jwt_required
 
 lessons_blueprint = Blueprint('lessons', __name__, url_prefix='/lessons')
@@ -27,16 +29,22 @@ def subject_individual_view(subject_id):
     if request.method == 'GET':
         return subject_detail_view(request, subject_id=subject_id)
     if request.method == 'DELETE':
-        return subject_delete_view(request, subject_id)
+        if g.user.has_permissions({'Administrator'}):
+            return subject_delete_view(request, subject_id)
+        raise UnauthorizedError()
     if request.method == 'PUT':
-        return subject_update_view(request, subject_id)
+        if g.user.has_permissions({'Administrator'}):
+            return subject_update_view(request, subject_id)
+        raise UnauthorizedError()
 
 
 @lessons_blueprint.route('/lesson', methods=['POST', 'GET'])
 @jwt_required()
 def lesson_list_or_create_view():
     if request.method == "POST":
-        return lesson_create(request)
+        if g.user.has_permissions({'Administrator'}):
+            return lesson_create(request)
+        raise UnauthorizedError()
     if request.method == "GET":
         return lesson_listing(request)
 
@@ -48,7 +56,11 @@ def lesson_detail_view(lesson_id):
         return lesson_detail(request, lesson_id=lesson_id)
 
     if request.method == 'PUT':
-        return lesson_update(request, lesson_id=lesson_id)
+        if g.user.has_permissions({'Administrator'}):
+            return lesson_update(request, lesson_id=lesson_id)
+        raise UnauthorizedError()
 
     if request.method == 'DELETE':
-        return lesson_delete(request, lesson_id=lesson_id)
+        if g.user.has_permissions({'Administrator'}):
+            return lesson_delete(request, lesson_id=lesson_id)
+        raise UnauthorizedError()
